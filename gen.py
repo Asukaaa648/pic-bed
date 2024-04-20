0be936467fee756bd3c32344448c16b6848566bd
Asukaaa <qingming648@gmail.com>
2024-04-20 13:21:31 +0800

    更新图片

---
+++ gen.py
@@ -0,0 +1,24 @@
+import os
+import csv
+
+def generate_urls(folder_path, base_url):
+    urls = []
+    for filename in os.listdir(folder_path):
+        if os.path.isfile(os.path.join(folder_path, filename)):
+            urls.append(base_url + filename)
+    return urls
+
+def write_to_csv(urls, output_file):
+    with open(output_file, 'w', newline='') as csvfile:
+        writer = csv.writer(csvfile)
+        for url in urls:
+            writer.writerow([url])
+
+if __name__ == "__main__":
+    base_url = "https://pic.my-nas8.top/covers/"
+    folder_path = "./imgs/covers/"
+    output_file = "url.csv"
+    
+    urls = generate_urls(folder_path, base_url)
+    write_to_csv(urls, output_file)
+    print("CSV success")
---
+++ update.bat
@@ -0,0 +1,5 @@
+chcp 65001
+git add .
+git commit -m "更新图片"
+git push
+pause
