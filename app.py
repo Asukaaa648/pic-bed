3e4211a6bcc32335d4651431c4076f7abaa7f2c6
Asukaaa <qingming648@gmail.com>
2024-05-26 08:37:25 +0800

    update

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, redirect
 import pandas as pd
 import random
 
@@ -6,7 +6,10 @@ app = Flask(__name__)
 
 # 读取url.csv文件
 def read_image_urls(file_path):
-    df = pd.read_csv(file_path)
+    # 读取CSV文件，没有标题行
+    df = pd.read_csv(file_path, header=None)
+    # 手动设置列名
+    df.columns = ['url']
     return df['url'].tolist()
 
 # 加载图片URL列表
@@ -15,9 +18,9 @@ image_urls = read_image_urls('url.csv')
 @app.route('/random-image', methods=['GET'])
 def get_random_image():
     if not image_urls:
-        return jsonify({'error': 'No images found'}), 404
+        return "No images found", 404
     random_url = random.choice(image_urls)
-    return jsonify({'url': random_url})
+    return redirect(random_url)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0',debug=True)
